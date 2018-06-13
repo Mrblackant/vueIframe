cbd234545a78ef798dbd2777bfd4ee17b89715f5
赵云 <zzlan@shinyway.com.cn>
2018-06-13 22:01:17 +0800

    暂存

--- src/permission.js
+++ src/permission.js
@@ -1,6 +1,19 @@
 import router from './router'
 import {constantRouterMap} from '@/router'
+ function getStr(str, cutStr) {
 
+var str_last = str.lastIndexOf(cutStr);
+
+var str_before = str.substring(0, str_last);
+
+//console.log(str_before);
+
+return str_before
+
+}
+
+
+    
 router.beforeEach((to, from, next) => {
 	if(localStorage.getItem('new')){
 			var c= JSON.parse(localStorage.getItem('new'))
@@ -10,8 +23,17 @@ router.beforeEach((to, from, next) => {
 		        }])
 			localStorage.removeItem('new')
 	      router.addRoutes(newRoutes)
-	       console.log()
-	       window.location.href='http://'+window.location.host+'/#'+c.path
+	       let hr=window.location.href
+	       hr =getStr(hr, "/")
+	       console.log(hr)
+	       // +c.path
+	       //  var ab =hr.lastIndexOf('//')
+	       // hr= replaceChat(hr,ab,"/")
+	       // console.log(hr)
+	       //  console.log(ab)
+	       // var b = hr.substring(a.lastIndexOf('//'))
+	       // hr =hr.replace('//','/')
+	       window.location.href=hr+c.path
 			next()
 
 			}
